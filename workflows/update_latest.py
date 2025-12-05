4268bc92a710472295bb7291db992cffbab0e27f
nuuuwan <nuuuwan@gmail.com>
2025-12-05 17:51:22 +0530

    local update_latest

--- workflows/update_latest.py
+++ workflows/update_latest.py
@@ -1,5 +1,6 @@
-from lk_weather import WeatherReport3h
+from lk_weather import ReadMe, WeatherReport3h
 
 if __name__ == "__main__":
     WeatherReport3h.list_latest_from_remote()
     WeatherReport3h.aggregate()
+    ReadMe().build()
