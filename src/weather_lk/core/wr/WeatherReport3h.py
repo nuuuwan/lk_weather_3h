477a75207e7fbb76f33745a07ca38ad1734906ab
nuuuwan <nuuuwan@gmail.com>
2025-12-05 17:09:09 +0530

    Add methods to retrieve and list JSON weather report files

--- src/weather_lk/core/wr/WeatherReport3h.py
+++ src/weather_lk/core/wr/WeatherReport3h.py
@@ -102,6 +102,28 @@ class WeatherReport3hReadMixin:
     def json_file(self) -> JSONFile:
         return JSONFile(self.json_file_path)
 
+    @classmethod
+    def __get_json_files__(cls):
+        json_files = []
+        for dirpath, _, filenames in os.walk(cls.DIR_DATA_WEATHER_REPORT):
+            for filename in filenames:
+                if filename.endswith(".json"):
+                    json_files.append(
+                        JSONFile(os.path.join(dirpath, filename))
+                    )
+        return json_files
+
+    @classmethod
+    def from_json_file(cls, json_file: JSONFile) -> "WeatherReport3h":
+        return cls(**json_file.read())
+
+    @classmethod
+    def list_all(cls) -> List["WeatherReport3h"]:
+        return [
+            cls.from_json_file(json_file)
+            for json_file in cls.__get_json_files__()
+        ]
+
 
 class WeatherReport3hWriteMixin:
     def write(self) -> bool:
