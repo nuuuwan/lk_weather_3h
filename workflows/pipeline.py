b74f107e7d861fd8021fdb034768ac9383b35589
nuuuwan <nuuuwan@gmail.com>
2025-12-05 17:07:25 +0530

    Remove unnecessary loop for printing weather reports in pipeline execution

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,6 +1,4 @@
 from weather_lk import WeatherReport3h
 
 if __name__ == "__main__":
-    wr3_list = WeatherReport3h.list_latest_from_remote()
-    for wr3 in wr3_list:
-        print(wr3)
+    WeatherReport3h.list_latest_from_remote()
