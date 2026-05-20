c409ec3c3adb1544c9bacfb517d4b4851bd4ac97
nuuuwan <nuuuwan@gmail.com>
2026-05-20 11:55:52 +0530

    Add additional Chrome options for headless browser execution

--- src/lk_weather/core/wr/WeatherReport3hRemoteMixin.py
+++ src/lk_weather/core/wr/WeatherReport3hRemoteMixin.py
@@ -13,6 +13,9 @@ class WeatherReport3hRemoteMixin:
 
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
         driver = webdriver.Chrome(options=options)
 
         try:
